673df0332cc8d140bf5804470f7993fc07d622f2
ansrl0107 <ansrl0107@gmail.com>
2018-11-06 21:26:20 +0900

    tmp

---
+++ src/routes/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <header>
+                    <input></input>
+                </header>
+                <section>
+                    <div className='container'>
+                        
+                    </div>
+                </section>
+            </React.Fragment>
+        )
+    }
+}
+export default Home;
