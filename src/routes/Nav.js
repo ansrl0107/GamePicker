b2e0651fb42b8f8d6a8f6aa5402ca6c08c312a01
ansrl0107 <ansrl0107@gmail.com>
2018-12-21 16:49:56 +0900

    tmp

--- src/routes/Nav.js
+++ src/routes/Nav.js
@@ -6,9 +6,9 @@ class Menu extends Component {
     render() {      
         return (
             <nav>
-                <Link to='/' className='icon' id='home'></Link>
-                <Link to='/talks' className='icon' id='talks'></Link>
-                <Link to='/games/all' className='icon' id='games'></Link>
+                <Link to='/' className='icon' id='home'>GamePicker</Link>
+                <Link to='/talks' className='icon' id='talks'>talks</Link>
+                <Link to='/games/all' className='icon' id='games'>games</Link>
                 <Link to='/manage/games' className='icon'>manage</Link>
             </nav>
         )
